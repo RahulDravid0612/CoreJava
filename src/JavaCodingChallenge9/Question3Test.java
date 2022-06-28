dc80aa261b7993ce0f9db73c172492677a05a93a
Rahul Dravid <dravid483@gmail.com>
2022-06-28 18:26:32 +0530

    CoreJava

---
+++ src/JavaCodingChallenge9/Question3Test.java
@@ -0,0 +1,8 @@
+package JavaCodingChallenge9;
+
+public class Question3Test {
+    public static void main(String[] args) {
+        Question3 b = new Question3();
+        b.print();
+    }
+}
---
+++ src/Opps/ClassandObject/Dogtest.java
@@ -0,0 +1,16 @@
+package Opps.ClassandObject;
+
+public class Dogtest {
+    public static void main(String[] args) {
+        Dog d=new Dog();
+        d.name="max";
+        d.age=6;
+        d.breed="poodle";
+        d.color="white";
+
+        System.out.println(d.name+" : "+d.age+" : "+d.breed+" : " +d.color);
+        d.bark();
+        d.eat();
+        d.wagtail();
+    }
+}
